373d1fd39d7ad469af260f012abf3cfad19b6c64
Gustavo Gambarini <gambarini@gmail.com>
2013-08-17 15:57:39 -0300

    Starting web site design

--- updateWeb.sh
+++ updateWeb.sh
@@ -3,3 +3,4 @@ cp -Rv ./src/main/webapp/angular ./target/postcenter-1.0-SNAPSHOT
 cp -Rv ./src/main/webapp/css ./target/postcenter-1.0-SNAPSHOT
 cp -Rv ./src/main/webapp/partials ./target/postcenter-1.0-SNAPSHOT
 cp -Rv ./src/main/webapp/index.html ./target/postcenter-1.0-SNAPSHOT
+cp -Rv ./src/main/webapp/js ./target/postcenter-1.0-SNAPSHOT
